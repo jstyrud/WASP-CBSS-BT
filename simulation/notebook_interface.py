3f241c46d8fa0d92a914a1e958520a6ea23c6443
Jonathan Styrud <jonathan.styrud@gmail.com>
2021-06-17 09:32:56 +0200

    Added stepping

--- simulation/notebook_interface.py
+++ simulation/notebook_interface.py
@@ -14,16 +14,29 @@ class Environment():
         self.seed = seed
         self.verbose = verbose
         self.fitness_coeff = fitness_coeff
+        self.world_interface = None
+        self.pytree = None
 
     def get_fitness(self, individual, show_world=False):
         """ Run the simulation and return the fitness """
-        world_interface = sm.Simulation(seed=self.seed)
-        pytree = PyTree(individual[:], behaviors=behaviors, world_interface=world_interface, verbose=self.verbose)
+        self.world_interface = sm.Simulation(seed=self.seed)
+        self.pytree = PyTree(individual[:], behaviors=behaviors, \
+            world_interface=self.world_interface, verbose=self.verbose)
 
         # run the Behavior Tree
-        ticks, _ = pytree.run_bt(show_world=show_world)
+        ticks, _ = self.pytree.run_bt(show_world=show_world)
 
-        return fitness_function.compute_fitness(world_interface, pytree, ticks, self.fitness_coeff)
+        return fitness_function.compute_fitness(self.world_interface, self.pytree, ticks, self.fitness_coeff)
+
+    def step(self, individual, show_world=False):
+        """ Run the simulation and return the fitness """
+        if self.world_interface is None:
+            self.world_interface = sm.Simulation(seed=self.seed)
+        if self.pytree is None:
+            self.pytree = PyTree(individual[:], behaviors=behaviors, \
+                world_interface=self.world_interface, verbose=self.verbose)
+
+        self.pytree.step_bt(show_world=show_world)
 
 def plot_individual(path, plot_name, individual):
     """ Saves a graphical representation of the individual """

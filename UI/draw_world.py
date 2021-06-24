39be5a49ee75b2caadd85041c3e1810c1557111f
matteoi <matteoi@kth.se>
2021-06-24 10:28:06 +0200

    Animation fixed.

--- UI/draw_world.py
+++ UI/draw_world.py
@@ -61,7 +61,7 @@ class Object:
 class WorldUI:
     """ User Interface that allows prints the world state """
 
-    def __init__(self):
+    def __init__(self, animate=False):
         """
         Initialize the static objects in the world.
         """
@@ -88,8 +88,9 @@ class WorldUI:
         self.item_l = Object(x=0, y=0, l=0.5, h=0.5, line='red', fill='red')
         self.item_h = Object(x=0, y=0, l=0.5, h=1, line='blue', fill='blue')
 
-        self.var_map = self.init_map()
-        self.var_tab = self.init_table()
+        if not animate:
+            self.var_map = self.init_map()
+            self.var_tab = self.init_table()
 
 
     def init_map(self):
@@ -171,7 +172,6 @@ class WorldUI:
             'R', fontweight='bold')
 
         return self.var_map
-
 
 
     def init_table(self):
@@ -302,18 +302,121 @@ class WorldUI:
         plt.close(self.figure)
 
 
-    def plot_world(self):
-        """
-        Plot the world
-        """
-        self.map_ax.plot()
-        self.text_ax.plot()
-        self.camera.snap()
-
-
     def animate(self, path='./animation.gif'):
         """
         Save an animated gif of the world, one frame per tick
         """
         animation = self.camera.animate()
         animation.save(path, writer='imagemagick')
+
+
+    def animate_state(self, world_state):
+        """
+        Function dedicated to the animation:
+        prints the world again from scratch with the state info.
+        """
+        # ----------------------- MAP -----------------------#
+         # add the Map
+        self.map_ax.add_patch(Rectangle(self.map.origin, self.map.length, self.map.height, \
+            edgecolor=self.map.line, facecolor=self.map.fill))
+
+        # add the Conveyors:
+        # - main gray band
+        # - black entry point
+        # - black band lines
+        # - label
+        self.map_ax.add_patch(Rectangle(self.conv_l.origin, self.conv_l.length, self.conv_l.height, \
+                                      edgecolor=self.conv_l.line, facecolor=self.conv_l.fill))
+        self.map_ax.add_patch(Rectangle((self.conv_l.origin[0]-1, self.conv_l.origin[1]-1), 1, 4, \
+            edgecolor='black', facecolor='black'))
+        for i in range(self.conv_l.length):
+            x_points = [self.conv_l.origin[0]+i, self.conv_l.origin[0]+i]
+            y_points = [self.conv_l.origin[1], self.conv_l.origin[1]+self.conv_l.height]
+            line = lines.Line2D(x_points, y_points, color='black', axes=self.map_ax)
+            self.map_ax.add_line(line)
+        self.map_ax.text(self.conv_l.origin[0]+0.5, self.conv_l.origin[1]-1.5, 'Conveyor LIGHT')
+
+        self.map_ax.add_patch(Rectangle(self.conv_h.origin, self.conv_h.length, self.conv_h.height,  \
+                                      edgecolor=self.conv_h.line, facecolor=self.conv_h.fill))
+        self.map_ax.add_patch(Rectangle((self.conv_h.origin[0]-1, self.conv_h.origin[1]-1), 1, 4, \
+            edgecolor='black', facecolor='black'))
+        for i in range(self.conv_h.length):
+            x_points = [self.conv_h.origin[0]+i, self.conv_h.origin[0]+i]
+            y_points = [self.conv_h.origin[1], self.conv_h.origin[1]+self.conv_h.height]
+            line = lines.Line2D(x_points, y_points, color='black', axes=self.map_ax)
+            self.map_ax.add_line(line)
+        self.map_ax.text(self.conv_h.origin[0]+0.5, self.conv_h.origin[1]+self.conv_h.height+1, 'Conveyor HEAVY')
+
+        # add the Delivery area
+        self.map_ax.add_patch(Rectangle(self.delivery.origin, self.delivery.length, self.delivery.height,  \
+            edgecolor=self.delivery.line, facecolor=self.delivery.fill))
+        self.map_ax.text(self.delivery.origin[0]-0.5, self.delivery.origin[1]-1, 'Delivery')
+
+        # add the Chargin stations
+        self.map_ax.add_patch(Rectangle(self.charge_c.origin, self.charge_c.length, self.charge_c.height, \
+            edgecolor=self.charge_c.line, facecolor=self.charge_c.fill))
+        self.map_ax.text(self.charge_c.origin[0]+0.1, self.charge_c.origin[1]-1, 'Charge 1')
+        self.map_ax.add_patch(Rectangle(self.charge_d.origin, self.charge_d.length, self.charge_d.height, \
+            edgecolor=self.charge_d.line, facecolor=self.charge_d.fill))
+        self.map_ax.text(self.charge_d.origin[0]-2, self.charge_d.origin[1]+2.5, 'Charge 2')
+
+        # add the Items
+        origin_lx = self.conv_l.origin[0] + self.conv_l.length - self.item_l.length*1.5
+        origin_ly = self.conv_l.origin[1] + 0.5*(self.conv_l.height - self.item_l.height)
+
+        for i in range(world_state.cnv_n_light):
+            self.item_l.set_origin(origin_lx - i, origin_ly)
+            self.map_ax.add_patch(Rectangle(self.item_l.origin, self.item_l.length, self.item_l.height, \
+                edgecolor=self.item_l.line, facecolor=self.item_l.fill))
+
+        origin_hx = self.conv_h.origin[0] + self.conv_h.length - self.item_h.length*1.5
+        origin_hy = self.conv_h.origin[1] + 0.5*(self.conv_h.height - self.item_h.height)
+        for i in range(world_state.cnv_n_heavy):
+            self.item_h.set_origin(origin_hx - i, origin_hy)
+            self.map_ax.add_patch(Rectangle(self.item_h.origin, self.item_h.length, self.item_h.height, \
+                edgecolor=self.item_h.line, facecolor=self.item_h.fill))
+
+        # add the Robot
+        pose = (world_state.robot_pos.x, world_state.robot_pos.y)
+        self.map_ax.add_patch(RegularPolygon(pose, 8, radius=1, orientation=math.pi/8, \
+            edgecolor='black', facecolor='paleturquoise'))
+        self.map_ax.text(pose[0]-0.4, pose[1]-0.2, 'R', fontweight='bold')
+
+
+        # ----------------------- TABLE -----------------------#
+        # add dummy patch like the map
+        self.text_ax.add_patch(Rectangle(self.map.origin, self.map.length/3, self.map.height, \
+            edgecolor='white', facecolor='white'))
+
+        self.text_ax.text(1, 14, 'robot pose')
+        self.text_ax.text(1, 13, 'battery lv')
+        self.text_ax.text(1, 12, 'carried weight')
+        self.text_ax.text(1, 11, 'carried light')
+        self.text_ax.text(1, 10, 'carried heavy')
+        self.text_ax.text(1, 9, 'heavy in conveyor')
+        self.text_ax.text(1, 8, 'light in conveyor')
+        self.text_ax.text(1, 7, 'delivered heavy')
+        self.text_ax.text(1, 6, 'delivered light')
+
+        self.var_tab = [None]*(len(TableVars))
+        self.var_tab[TableVars.ROBOT_POSE] = self.text_ax.text(7, 14, pose)
+        self.var_tab[TableVars.BATTERY_LV] = self.text_ax.text(7, 13, str(world_state.battery_level))
+        self.var_tab[TableVars.CARRIED] = self.text_ax.text(7, 12, str(world_state.carried_weight))
+        self.var_tab[TableVars.CARRIED_L] = self.text_ax.text(7, 11, str(world_state.carried_light))
+        self.var_tab[TableVars.CARRIED_H] = self.text_ax.text(7, 10, str(world_state.carried_heavy))
+        self.var_tab[TableVars.CONV_H] = self.text_ax.text(7, 9, str(world_state.cnv_n_heavy))
+        self.var_tab[TableVars.CONV_L] = self.text_ax.text(7, 8, str(world_state.cnv_n_light))
+        self.var_tab[TableVars.DEL_H] = self.text_ax.text(7, 7, str(world_state.delivered_heavy))
+        self.var_tab[TableVars.DEL_L] = self.text_ax.text(7, 6, str(world_state.delivered_light))
+
+        # Hide axes
+        self.text_ax.get_xaxis().set_visible(False)
+        self.text_ax.get_yaxis().set_visible(False)
+        # Hide axes border
+        self.text_ax.axis('off')
+
+
+        # ----------------------- PLOT -----------------------#
+        self.map_ax.plot()
+        self.text_ax.plot()
+        self.camera.snap()
